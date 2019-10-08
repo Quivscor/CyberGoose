d5bbb1e6828fd7b830fa9d738886fcf2822700d8
Quivscor <quivscor@gmail.com>
2019-10-08 13:44:25 +0200

    Add simple main menu scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private PlayerScore score;
+
+    public static GameManager Instance;
+    void Start()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore
+{
+    public int points { get; set; }
+    public float time { get; set; }
+}
