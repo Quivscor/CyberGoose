6a1d8c78b7d8a010af919723affcd355b7b66412
Quivscor <quivscor@gmail.com>
2019-10-08 17:32:13 +0200

    Add Mini Game Template

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+///<summary>
+///Holds information about amount of points earned, time passed since the start of a run
+///and amount of mini games that were succesfully completed.
+///</summary>
 public class PlayerScore
 {
     public int points { get; set; }
     public float time { get; set; }
+    public int gamesWon { get; set; }
 }
