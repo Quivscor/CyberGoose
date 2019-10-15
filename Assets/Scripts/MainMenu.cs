be217d5f7f254bfc28afcad54fbca192f133180c
Quivscor <quivscor@gmail.com>
2019-10-15 12:07:19 +0200

    Main menu proceeds to the next mini game correctly

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,6 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(GameManager.Instance.GetNextMiniGame());
     }
 }
