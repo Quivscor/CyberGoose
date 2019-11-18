12479e20861c5a6dac950b6cb01fd2c02e041a59
TomczykIII <kusmierekt97@gmail.com>
2019-11-18 13:39:27 +0100

    dupa

---
+++ Assets/LiquidSpammer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidSpammer : MonoBehaviour
+{
+    public GameObject liquid;
+    public float respawn_speed=1;
+    public float used_time;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        used_time += Time.deltaTime;
+        if(used_time>respawn_speed)
+        {
+            Instantiate(liquid, new Vector3(Random.value * 10-5, transform.position.y-1, 0), Quaternion.identity,this.transform);
+            used_time = 0;
+        }
+        
+    }
+}
