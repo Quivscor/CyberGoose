2b71eb84a89d82b009d8c516b8887b7f15b1f0e7
Quivscor <quivscor@gmail.com>
2019-11-18 16:00:18 +0100

    Add 10 catchphrases to the system

--- Assets/PopupController.cs
+++ Assets/PopupController.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopupController : MonoBehaviour
 {
     [SerializeField]
     private int popupCount;
 
+    public GameObject popupPrefab;
+
     private List<GameObject> popups;
+    public List<string> catchphrases;
+
+    public List<GameObject> popupTracker;
+
+    private void Start()
+    {
+        for(int i = 0; i < popupCount; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-2, 2), Random.Range(-3, 3));
+            GameObject popup = GameObject.Instantiate<GameObject>(popupPrefab, position, Quaternion.identity, GameObject.Find("Canvas").gameObject.transform);
+            popup.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = catchphrases[Random.Range(0, catchphrases.Count)];
+            popupTracker.Add(popup);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        popupTracker.RemoveAll(x => x == null);
+        if (popupTracker.Count == 0)
+            GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().OnWin();
+    }
 }
