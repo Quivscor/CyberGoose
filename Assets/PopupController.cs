d12c35fbf05774b3cde9939d46f947283cccf1a3
Quivscor <quivscor@gmail.com>
2019-11-18 15:12:41 +0100

    Add base popup mechanic

---
+++ Assets/PopupController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupController : MonoBehaviour
+{
+    [SerializeField]
+    private int popupCount;
+
+    private List<GameObject> popups;
+}
