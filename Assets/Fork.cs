126ef4e6b724e5bb127fba787940c00fa85c6863
Quivscor <quivscor@gmail.com>
2019-12-17 02:15:58 +0100

    Add WorkSafety Game

---
+++ Assets/Fork.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fork : MonoBehaviour
+{
+    public GameObject fork;
+    public GameObject particle;
+    bool loss = false;
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            fork.transform.Translate(Vector3.left);
+            particle.SetActive(true);
+            StartCoroutine("Wait", 1);
+        }
+        if (loss)
+            GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().OnDefeat();
+    }
+
+    private IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        loss = true;
+        
+    }
+}
