24c7c4078731f4629293b8586a23bc062a100d23
Quivscor <quivscor@gmail.com>
2019-12-17 01:40:36 +0100

    Add Electronics game, conceptual stage

---
+++ Assets/CableConnector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CableConnector : MonoBehaviour
+{
+    private CableUnit[,] cables = new CableUnit[4,4];
+    private void Start()
+    {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                cables[i,j] = this.transform.GetChild(i * 4 + j).GetComponent<CableUnit>();
+    }
+
+    public void CheckWinCondition()
+    {
+        if (cables[0, 0].left && cables[0, 0].right)
+            if (cables[0, 1].left && cables[0, 1].down)
+                if (cables[1, 1].up && cables[1, 1].right)
+                    if (cables[1, 2].left && cables[1, 2].right)
+                        if (cables[1, 3].left && cables[1, 3].down)
+                            if (cables[2, 3].up && cables[2, 3].down)
+                                if (cables[3, 3].up && cables[3, 3].right)
+                                {
+                                    GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().OnWin();
+                                }
+    }
+}
---
+++ Assets/CableUnit.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CableUnit : MonoBehaviour
+{
+    private static CableConnector connector;
+
+    private Image image;
+    private Button button;
+    public float rotationRate = 1000;
+
+    public int ID;
+
+    public bool up;
+    public bool down;
+    public bool left;
+    public bool right;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        button = GetComponent<Button>();
+
+        if (connector == null)
+            connector = GetComponentInParent<CableConnector>();
+    }
+
+    public void RotateCable()
+    {
+        RotateImage();
+        RotateConnections();
+    }
+
+    private void RotateImage()
+    {
+        StartCoroutine("RotatePicOverTimeAndLockButton");
+    }
+
+    private IEnumerator RotatePicOverTimeAndLockButton()
+    {
+        float rotation = 90.0f;
+        button.interactable = false;
+        while(rotation > 0)
+        {
+            image.rectTransform.Rotate(0, 0, -Time.deltaTime * rotationRate);
+            Debug.Log(-Time.deltaTime * rotationRate);
+            rotation -= Time.deltaTime * rotationRate;
+            yield return new WaitForEndOfFrame();
+        }
+        button.interactable = true;
+
+        connector.CheckWinCondition();
+    }
+
+    private void RotateConnections()
+    {
+        bool tempUp, tempDown, tempLeft, tempRight;
+        tempUp = tempDown = tempLeft = tempRight = false;
+
+        if (up)
+            tempRight = true;
+        if (right)
+            tempDown = true;
+        if (down)
+            tempLeft = true;
+        if (left)
+            tempUp = true;
+
+        up = tempUp;
+        down = tempDown;
+        left = tempLeft;
+        right = tempRight;
+    }
+}
