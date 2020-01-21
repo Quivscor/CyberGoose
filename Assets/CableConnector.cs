6a523561471abc55dcef0de16158aeab7a28d983
Quivscor <quivscor@gmail.com>
2020-01-21 07:07:29 +0100

    Add variety to the electronics mini game

--- Assets/CableConnector.cs
+++ Assets/CableConnector.cs
@@ -4,25 +4,108 @@ using UnityEngine;
 
 public class CableConnector : MonoBehaviour
 {
-    private CableUnit[,] cables = new CableUnit[4,4];
+    private CableUnit[,] cables = new CableUnit[3,3];
+
+    private int variants = 3;
+    private int variant;
     private void Start()
     {
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                cables[i,j] = this.transform.GetChild(i * 4 + j).GetComponent<CableUnit>();
+        variant = Random.Range(0, variants);
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                cables[i,j] = this.transform.GetChild(i * 3 + j).GetComponent<CableUnit>();
+
+        if(variant == 0)
+        {
+            cables[0, 0].SetStartConnections(0);
+            cables[0, 1].SetStartConnections(3);
+            cables[0, 2].SetStartConnections(6);
+            cables[1, 0].SetStartConnections(0);
+            cables[1, 1].SetStartConnections(4);
+            cables[1, 2].SetStartConnections(2);
+            cables[2, 0].SetStartConnections(1);
+            cables[2, 1].SetStartConnections(5);
+            cables[2, 2].SetStartConnections(7);
+        }
+        else if(variant == 1)
+        {
+            cables[0, 0].SetStartConnections(6);
+            cables[0, 1].SetStartConnections(0);
+            cables[0, 2].SetStartConnections(2);
+            cables[1, 0].SetStartConnections(0);
+            cables[1, 1].SetStartConnections(5);
+            cables[1, 2].SetStartConnections(2);
+            cables[2, 0].SetStartConnections(4);
+            cables[2, 1].SetStartConnections(9);
+            cables[2, 2].SetStartConnections(0);
+        }
+        else if(variant == 2)
+        {
+            cables[0, 0].SetStartConnections(2);
+            cables[0, 1].SetStartConnections(3);
+            cables[0, 2].SetStartConnections(2);
+            cables[1, 0].SetStartConnections(9);
+            cables[1, 1].SetStartConnections(6);
+            cables[1, 2].SetStartConnections(7);
+            cables[2, 0].SetStartConnections(4);
+            cables[2, 1].SetStartConnections(3);
+            cables[2, 2].SetStartConnections(4);
+        }
     }
 
     public void CheckWinCondition()
     {
-        if (cables[0, 0].left && cables[0, 0].right)
-            if (cables[0, 1].left && cables[0, 1].down)
-                if (cables[1, 1].up && cables[1, 1].right)
-                    if (cables[1, 2].left && cables[1, 2].right)
-                        if (cables[1, 3].left && cables[1, 3].down)
-                            if (cables[2, 3].up && cables[2, 3].down)
-                                if (cables[3, 3].up && cables[3, 3].right)
-                                {
-                                    GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().OnWin();
-                                }
+        switch(variant)
+        {
+            case 0:
+                if(cables[0, 0].left && cables[0, 0].right &&
+                    cables[0, 1].left && cables[0, 1].down &&
+                    cables[1,1].up && cables[1,1].right &&
+                    cables[1,2].left && cables[1,2].down &&
+                    cables[2,2].up && cables[2,2].right)
+                {
+                    GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().OnWin();
+                }
+                break;
+            case 1:
+                if(cables[0,0].left && cables[0,0].down &&
+                    cables[1,0].up && cables[1,0].down &&
+                    cables[2,0].up && cables[2,0].right &&
+                    cables[2,1].left && cables[2,1].right &&
+                    cables[2,2].left && cables[2,2].right)
+                {
+                    GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().OnWin();
+                }
+                else if(cables[0, 0].left && cables[0,0].right &&
+                    cables[0,1].left && cables[0,1].right &&
+                    cables[0, 2].left && cables[0,2].down &&
+                    cables[1,2].up && cables[1,2].left &&
+                    cables[1,1].right && cables[1,1].down &&
+                    cables[2,1].up && cables[2,1].right &&
+                    cables[2, 2].left && cables[2, 2].right)
+                {
+                    GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().OnWin();
+                }
+                break;
+            case 2:
+                if(cables[0,0].left && cables[0,0].down &&
+                    cables[1, 0].up && ((cables[1,0].right && 
+                    cables[1,1].left && cables[1,1].right &&
+                    cables[1,2].left && cables[1,2].down &&
+                    cables[2,2].up && cables[2,2].right) ||
+                    (cables[1,0].down && cables[2,0].up &&
+                    cables[2,0].right && cables[2,1].left &&
+                    cables[2,1].up && cables[1,1].down &&
+                    cables[1,1].up && cables[0,1].down &&
+                    cables[0,1].right && cables[0,2].left &&
+                    cables[0,2].down && cables[1,2].up &&
+                    cables[1,2].down && cables[2,2].up &&
+                    cables[2,2].right)))
+                {
+                    GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().OnWin();
+                }
+                break;
+        }
     }
 }
--- Assets/CableUnit.cs
+++ Assets/CableUnit.cs
@@ -7,6 +7,10 @@ public class CableUnit : MonoBehaviour
 {
     private static CableConnector connector;
 
+    public Sprite line;
+    public Sprite curve;
+    public Sprite T;
+
     private Image image;
     private Button button;
     public float rotationRate = 1000;
@@ -18,13 +22,117 @@ public class CableUnit : MonoBehaviour
     public bool left;
     public bool right;
 
-    private void Start()
+    private void Awake()
     {
         image = GetComponent<Image>();
         button = GetComponent<Button>();
 
         if (connector == null)
             connector = GetComponentInParent<CableConnector>();
+    }
+
+    /// <summary>
+    /// Cases:
+    /// 0 - horizontal bar
+    /// 1 - vertical bar
+    /// 2 - left-down curve
+    /// 3 - left-up curve
+    /// 4 - right-up curve
+    /// 5 - right-down curve
+    /// 6 - down-horizontal T
+    /// 7 - left-vertical T
+    /// 8 - right-vertical T
+    /// 9 - up-horizontal T
+    /// </summary>
+    /// <param name="variant"></param>
+    public void SetStartConnections(int variant)
+    {
+        switch(variant)
+        {
+            case 0: //horizontal bar
+                image.sprite = line;
+                left = true;
+                right = true;
+                up = false;
+                down = false;
+                break;
+            case 1: //vertical bar
+                image.sprite = line;
+                RotatePicInstant();
+                up = true;
+                down = true;
+                left = false;
+                right = false;
+                break;
+            case 2: //left-down curve
+                image.sprite = curve;
+                left = true;
+                down = true;
+                up = false;
+                right = false;
+                break;
+            case 3: //left-up curve
+                image.sprite = curve;
+                RotatePicInstant();
+                left = true;
+                up = true;
+                down = false;
+                right = false;
+                break;
+            case 4: //right-up curve
+                image.sprite = curve;
+                RotatePicInstant();
+                RotatePicInstant();
+                up = true;
+                right = true;
+                left = false;
+                down = false;
+                break;
+            case 5: //right-down curve
+                image.sprite = curve;
+                RotatePicInstant();
+                RotatePicInstant();
+                RotatePicInstant();
+                right = true;
+                down = true;
+                left = false;
+                up = false;
+                break;
+            case 6: //down-horizontal T
+                image.sprite = T;
+                RotatePicInstant();
+                RotatePicInstant();
+                down = true;
+                left = true;
+                right = true;
+                up = false;
+                break;
+            case 7: //left-vertical T
+                image.sprite = T;
+                RotatePicInstant();
+                RotatePicInstant();
+                RotatePicInstant();
+                left = true;
+                up = true;
+                down = true;
+                right = false;
+                break;
+            case 8: //right-vertical T
+                image.sprite = T;
+                RotatePicInstant();
+                right = true;
+                up = true;
+                down = true;
+                left = false;
+                break;
+            case 9: //up-horizontal T
+                image.sprite = T;
+                up = true;
+                right = true;
+                left = true;
+                down = false;
+                break;
+        }
     }
 
     public void RotateCable()
@@ -36,6 +144,11 @@ public class CableUnit : MonoBehaviour
     private void RotateImage()
     {
         StartCoroutine("RotatePicOverTimeAndLockButton");
+    }
+
+    private void RotatePicInstant()
+    {
+        image.rectTransform.Rotate(0, 0, -90.0f);
     }
 
     private IEnumerator RotatePicOverTimeAndLockButton()
